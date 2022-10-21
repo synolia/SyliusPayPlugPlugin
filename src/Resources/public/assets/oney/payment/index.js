ad442c2603673be3742cf4f2880be702d8d7c42a
Alexandre Béranger <alexandre.beranger@synolia.com>
2022-10-21 14:53:17 +0200

    [SYL-188] [FRONT] [WIP] Inté. Apple Pay : - suppr. label total apple pay pour test sur Mac - ajout trads. - màj assets compilés

--- src/Resources/public/assets/oney/payment/index.js
+++ src/Resources/public/assets/oney/payment/index.js
@@ -283,7 +283,7 @@ var Payment = {
                                 error: function(XHR, status, error) {
                                     console.log(XHR, status, error);
                                     session.abort();
-                                    window.location.reload();
+                                //window.location.reload();
                                 }
                             });
                         case 1:
@@ -321,13 +321,13 @@ var Payment = {
                         window.location.href = authorization.data.returnUrl;
                     } catch (err) {
                         console.error(err);
-                        window.location.reload();
+                    //window.location.reload();
                     }
                 },
                 error: function(XHR, status, error) {
                     console.log(XHR, status, error);
                     session.abort();
-                    window.location.reload();
+                //window.location.reload();
                 }
             });
         };
@@ -346,7 +346,7 @@ var Payment = {
                 },
                 error: function(XHR, status, error) {
                     console.log(XHR, status, error);
-                    window.location.reload();
+                //window.location.reload();
                 }
             });
         };
